f30812c9a76cdae6b22b8ae64aacf84d810bfbd8
kailash29 <kailash.vishnoi@incture.com>
2017-04-03 19:41:49 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var http = require('http');
+exports.serverstart = finction(){
+	http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(3001, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:3001/');
+}
+exports.printMsg = function() {
+  console.log("This is a message from the demo package");
+}
+
